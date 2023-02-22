fa5c6d3988b056e61644c66ba4858ae778b05d9c
Roman <kovalchuk.roman03@gmail.com>
2023-02-22 20:56:16 +0200

    Added spring context xml configuration, added application.properties file, added simple slf4j logger

--- src/main/java/com/epam/mentoring/nosql/task/manager/app/ConsoleTaskManagerApplication.java
+++ src/main/java/com/epam/mentoring/nosql/task/manager/app/ConsoleTaskManagerApplication.java
@@ -1,8 +1,15 @@
 package com.epam.mentoring.nosql.task.manager.app;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+@Slf4j
 public class ConsoleTaskManagerApplication {
 
     public static void main(String... args) {
-        System.out.println( "Hello World!" );
+
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:services.xml");
+        context.start();
+        log.info("ConsoleTaskManagerApplication context has been started!");
     }
 }
