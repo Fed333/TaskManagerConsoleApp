8c410b0c4cb22e15dfed0a399dea69b62cea5f23
Roman <kovalchuk.roman03@gmail.com>
2023-02-22 20:41:18 +0200

    Initial commit

---
+++ src/main/java/com/epam/mentoring/nosql/task/manager/app/ConsoleTaskManagerApplication.java
@@ -0,0 +1,8 @@
+package com.epam.mentoring.nosql.task.manager.app;
+
+public class ConsoleTaskManagerApplication {
+
+    public static void main(String... args) {
+        System.out.println( "Hello World!" );
+    }
+}
