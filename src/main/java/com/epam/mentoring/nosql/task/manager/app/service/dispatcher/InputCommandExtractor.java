7c3a2fbf509ce2d8a079d219e00181a693f702db
Roman <kovalchuk.roman03@gmail.com>
2023-02-25 15:23:38 +0200

    Added input command extractor parser, with unit tests coverage

---
+++ src/main/java/com/epam/mentoring/nosql/task/manager/app/service/dispatcher/InputCommandExtractor.java
@@ -0,0 +1,25 @@
+package com.epam.mentoring.nosql.task.manager.app.service.dispatcher;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * Class designed for extracting command and command params from the user input.
+ * @author Roman_Kovalchuk
+ * */
+public class InputCommandExtractor {
+
+    public String extractCommand(String input){
+        return input.substring(0, input.indexOf(' '));
+    }
+
+    public Map<String, Object> extractParams(String input) {
+        String[] params = input.substring(input.indexOf(' ')).trim().split("(?= --)");
+        return Arrays.stream(params)
+                .map(String::trim)
+                .map(param -> param.split("=", 2))
+                .collect(Collectors.toMap(p -> p[0], p -> p.length == 2 ? p[1] : p[0]));
+    }
+
+}
---
+++ src/test/java/com/epam/mentoring/nosql/task/manager/app/service/dispatcher/InputCommandExtractorTest.java
@@ -0,0 +1,52 @@
+package com.epam.mentoring.nosql.task.manager.app.service.dispatcher;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Map;
+
+public class InputCommandExtractorTest {
+
+    private InputCommandExtractor extractor;
+
+    @Before
+    public void setUp() {
+        extractor = new InputCommandExtractor();
+    }
+
+    @Test
+    public void extractCommand_shouldExtractToFirstSpace() {
+        String expected = "command";
+
+        String actual = extractor.extractCommand("command  --param1 --param2 --param3");
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void extractParams_shouldExtractFlags() {
+        Map<String, Object> expected = Map.of(
+                "--param1", "--param1",
+                "--param2", "--param2",
+                "--param3", "--param3"
+        );
+
+        Map<String, Object> actual = extractor.extractParams("command  --param1 --param2 --param3");
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void extractParams_shouldExtractFlagParameters() {
+        Map<String, Object> expected = Map.of(
+                "--param1", "param1",
+                "--param2", "param2",
+                "--param3", "param3"
+        );
+
+        Map<String, Object> actual = extractor.extractParams("command  --param1=param1 --param2=param2 --param3=param3");
+
+        Assert.assertEquals(expected, actual);
+    }
+}
